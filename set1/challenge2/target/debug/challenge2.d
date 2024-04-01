88ba8a2514fc85bda18113a4cf23f315b5cd09f6
0x79de <0x79de@gmail.com>
2024-04-01 19:39:51 +0300

    add challenge 2

--- set1/challenge2/target/debug/challenge2.d
+++
@@ -1,1 +0,0 @@
-/home/ozgur/code/rust/cryptopals/set1/challenge2/target/debug/challenge2: /home/ozgur/code/rust/cryptopals/set1/challenge2/src/main.rs
--- set1/challenge2/target/debug/deps/challenge2-dd4d52aeed8ddbdf.d
+++
@@ -1,5 +0,0 @@
-/home/ozgur/code/rust/cryptopals/set1/challenge2/target/debug/deps/challenge2-dd4d52aeed8ddbdf.rmeta: src/main.rs
-
-/home/ozgur/code/rust/cryptopals/set1/challenge2/target/debug/deps/challenge2-dd4d52aeed8ddbdf.d: src/main.rs
-
-src/main.rs:
