9ee264d107be71f2dab09948499029310f41a8f8
0x79de <0x79de@gmail.com>
2024-10-21 10:57:44 +0300

    Update

--- solutions/set1/challenge1/src/main.rs
+++ solutions/set1/challenge1/src/main.rs
@@ -1,10 +1,10 @@
-use base64;
+use base64::{Engine as _, engine::general_purpose::STANDARD};
 use hex;
 
 fn main() {
     let hex_encoded_text = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let decoded_bytes = hex::decode(hex_encoded_text).expect("Failed to decode hex");
-    let base64_encoded_text = base64::encode(&decoded_bytes);
+    let base64_encoded_text = STANDARD.encode(&decoded_bytes);
 
     println!("{}", base64_encoded_text);
 }
